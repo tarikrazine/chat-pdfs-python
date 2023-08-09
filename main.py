5279ed31036b987ff961809555e8c68f7dbe15f4
tarik razine <tarikrazine.com@gmail.com>
2023-08-09 01:16:55 +0100

    feat: persist

--- main.py
+++ main.py
@@ -8,65 +8,57 @@ from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
-import pickle
 import os
 
-store_name = "vectorstore"
+load_dotenv()
 
 
-def get_pdf_text(pdf_docs):
+def get_pdf_text(pdf_files):
     text = ""
-    for pdf in pdf_docs:
+
+    for pdf in pdf_files:
         pdf_reader = PdfReader(pdf)
+
         for page in pdf_reader.pages:
             text += page.extract_text()
+
     return text
 
 
-def get_text_chunks(text):
+def get_text_chunks(raw_text):
     text_splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=1000,
         chunk_overlap=200,
         length_function=len
     )
-    chunks = text_splitter.split_text(text)
+
+    chunks = text_splitter.split_text(raw_text)
+
     return chunks
 
 
 def get_vectorstore(text_chunks):
-
-    if os.path.exists(f"{store_name}.pkl"):
-        with open(f"{store_name}.pkl", "rb") as f:
-            Vectorstore = pickle.load(f)
-        st.write('Embeddings loaded from the disk')
-    else:
-        embeddings = OpenAIEmbeddings()
-        Vectorstore = FAISS.from_texts(text_chunks, embeddings)
-        with open(f"{store_name}.pkl", "wb") as f:
-            pickle.dump(Vectorstore, f)
-
-    return Vectorstore
+    embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
 
 
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
+    llm = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
 
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
-
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vectorstore.as_retriever(),
         memory=memory
     )
-
     return conversation_chain
 
 
-def handle_userinput(user_question):
+def handle_user_input(user_question):
     response = st.session_state.conversation({'question': user_question})
-
     st.session_state.chat_history = response['chat_history']
 
     for i, message in enumerate(st.session_state.chat_history):
@@ -79,9 +71,9 @@ def handle_userinput(user_question):
 
 
 def main():
-    load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon=":books:")
+
+    st.set_page_config(page_title="Chat with multiple PDF's 📚")
+
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
@@ -89,29 +81,28 @@ def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents:")
+    st.header("Chat with multiple PDF's 📚")
+
+    user_question = st.text_input("Ask question about your documents")
 
     if user_question:
-        handle_userinput(user_question)
+        handle_user_input(user_question)
 
     with st.sidebar:
         st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
 
-        if os.path.exists(f"{store_name}.pkl"):
-            st.write('Already your documents Embedded Click process to start')
+        pdf_files = st.file_uploader(
+            "Upload your PDF's here and click on 'Process'", accept_multiple_files=True)
 
         if st.button("Process"):
-            with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
+            with st.spinner("Processing..."):
+                # Get pdf text
+                raw_text = get_pdf_text(pdf_files)
 
-                # get the text chunks
+                # Get the text chunks
                 text_chunks = get_text_chunks(raw_text)
 
-                # create vector store
+                # Create vector store
                 vectorstore = get_vectorstore(text_chunks)
 
                 # create conversation chain
@@ -119,5 +110,5 @@ def main():
                     vectorstore)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
